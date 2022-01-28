e82dd5264a81ac0256a9b1e546c1e5031582a835
Luiz Pedro <luizpedro.pedrosilva@gmail.com>
2022-01-28 08:06:10 -0300

    atualização

--- consulta-cep/src/App.js
+++ consulta-cep/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="title">Consultar CEP:</h1>
+      
+      <div className="container">
+        <input type="text" placeholder="Digite um CEP"/>
+        <button className="btnsearch" >Consultar</button>
+      </div>
+      
     </div>
   );
 }
