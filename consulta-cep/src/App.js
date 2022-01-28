5fed7faa11e0f07e952464f22f2a7f8241f365a8
Luiz Pedro <luizpedro.pedrosilva@gmail.com>
2022-01-28 09:35:46 -0300

    estrutura-estilo

--- consulta-cep/src/App.js
+++ consulta-cep/src/App.js
@@ -1,14 +1,29 @@
+//inportar icone de pesquisa da lib instalada
+import {FiSearch} from 'react-icons/fi'
 
+//inporte do css style
+import './style.css'; 
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="title">Consultar CEP:</h1>
+    <div className="container">
+      <h1 className="title">Consultar CEP</h1>
       
-      <div className="container">
+      <div className="containerInput">
         <input type="text" placeholder="Digite um CEP"/>
-        <button className="btnsearch" >Consultar</button>
+
+        <button className="btnsearch" ><FiSearch size={25} color="#FFF"/></button>
+
       </div>
+      <section className='main'>
+        <h2>CEP:  72420290</h2>
+        
+        <span>Rua</span>
+        <span>Complemento</span>
+        <span>Bairro</span>
+        <span>cidade</span>
+
+      </section>
       
     </div>
   );
